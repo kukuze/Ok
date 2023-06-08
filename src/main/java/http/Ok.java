5f50710a883c7cdb0fb492dc54f55d74e9385b16
18043821456 <1092565192@qq.com>
2023-06-08 13:36:52 +0800

    无关痛痒

--- src/main/java/http/Ok.java
+++ src/main/java/http/Ok.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import okhttp3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
@@ -24,6 +26,7 @@ import static http.utils.Url.mapToEncodedUrl;
 
 
 public class Ok {
+    protected final static Logger log = LoggerFactory.getLogger(Ok.class);
     private static volatile OkHttpClient okHttpClient = null;
     private Map<String, String> headerMap;
     private Map<String, String> paramMap;
@@ -283,7 +286,7 @@ public class Ok {
                                 JSONObject json = JSONObject.parseObject(result);
                                 resultMap.put(index,json);
                             } catch (Exception e) {
-
+                                log.error(e.getMessage());
                             }finally {
                                 latch.countDown();
                             }
