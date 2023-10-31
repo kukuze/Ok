3baa826bd5de6b7dd723c569e74c15b750aa4a95
18043821456 <1092565192@qq.com>
2023-10-31 10:29:42 +0800

    Revert "完善日志框架 windows环境打zip  linux打gz"
    This reverts commit de165ca7c37d63b12d292fc17e2899cbc8becf8f.

--- src/main/java/http/utils/Gain.java
+++ src/main/java/http/utils/Gain.java
@@ -1,10 +1,7 @@
 package http.utils;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
-import java.util.List;
 
 /**
  * @author yjz
@@ -18,15 +15,6 @@ public class Gain {
 
     public static JSONArray getA(String res){
         return JSONObject.parseObject(res).getJSONArray("data");
-    }
-    public static <T> T getEntity(String res, Class<T> clazz) {
-        JSONObject jsonObject = JSON.parseObject(res).getJSONObject("data");
-        return JSON.toJavaObject(jsonObject, clazz);
-    }
-
-    public static <T> List<T> getList(String res, Class<T> clazz) {
-        JSONArray jsonArray = JSON.parseObject(res).getJSONArray("data");
-        return JSON.parseArray(jsonArray.toJSONString(), clazz);
     }
 
 }
