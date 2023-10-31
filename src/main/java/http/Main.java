c3fddd40d6abce57cb5345f592ad534162c13180
18043821456 <1092565192@qq.com>
2023-10-31 10:31:33 +0800

    结果获取能够直接指定类型

--- src/main/java/http/Main.java
+++ src/main/java/http/Main.java
@@ -5,7 +5,7 @@ package http;
  * @date 2023/5/10$ 11:26$
  * @description:
  */
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println(Ok.builder().url("http://114.242.29.130:10161/bh5000/node/info").get().sync());
     }
